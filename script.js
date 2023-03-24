3982b9f0c2bec53b9e16b2e0784ffbb0471a408a
afthab123456 <100578994+afthab123456@users.noreply.github.com>
2023-03-24 09:52:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const popup = document.querySelector('.Search');
+function openSearch() {
+  popup.classList.add('open');
+}
+function closeSearch() {
+  popup.classList.remove('open');
+}
+
+const bookmark = document.querySelector('.bookmark');
+function openBookmark() {
+  bookmark.classList.add('open');
+}
+function closeBookmark() {
+  bookmark.classList.remove('open');
+}
